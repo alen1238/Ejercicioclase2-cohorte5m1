1a2244a6736ae57772240d50deed85fe5fc9f2f7
Alfonso Lara <orozcoenrique159@gmail.com>
2025-10-16 21:36:40 -0500

    funcionalidad agregada

--- src/App.java
+++ src/App.java
@@ -2,5 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola mundo Dev Senior!");
         System.out.println("Modificación para commit");
+        System.out.println("Modificación 1 para nueva rama");
+        System.out.println("funcionalidad agregada en rama nueva-rama");
     }
 }
